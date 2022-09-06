d509d4d93b72cdc949ab83c72524f1c5ec81b0a9
Uxzylon <anthony17j@gmail.com>
2022-09-06 14:03:55 +0200

    1.19.1 Support

--- src/main/java/com/gmail/anthony17j/creativeworld/mixin/NetherPortalMixin.java
+++ src/main/java/com/gmail/anthony17j/creativeworld/mixin/NetherPortalMixin.java
@@ -13,7 +13,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public abstract class NetherPortalMixin {
     @Shadow public abstract World getEntityWorld();
 
-    @Inject(at = @At("HEAD"), method = "tickNetherPortal", cancellable = true)
+    @Inject(at = @At("HEAD"), method = "tickPortal", cancellable = true)
     public void tickNetherPortal(CallbackInfo ci) {
         if (this.getEntityWorld().getRegistryKey() == Main.CREATIVE_KEY) {
             ci.cancel();
